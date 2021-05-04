fb2b15674bf2417b1558676671b1562658e329b2
LE VAN TAN <72194651+levantanfc11@users.noreply.github.com>
2021-05-04 10:16:27 +0700

    Add files via upload

---
+++ baitapcay.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from binarytree import build
+values = [7, 3, 2, 6, 9, None, 1, 5, 8]
+root = build(values)
+print(root)
+print(root.preorder) # duyệt cây theo thứ tự trước
+print(root.postorder) # duyệt cây theo thứ tự sau
+
+
+# In[ ]:
+
+
+
+
